a6ad6ce9d5287dea98fff7bec1dfdb1f867e4c7a
SangYong-Jeong <mmaf@naver.com>
2021-11-25 00:01:21 +0900

    redux-saga 학습

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -1,23 +1,48 @@
 import { createAction, handleActions } from 'redux-actions';
+import {
+  delay,
+  put,
+  takeEvery,
+  takeLatest,
+  select,
+  throttle,
+} from 'redux-saga/effects';
 
 const INCREASE = 'counter/INCREASE';
 const DECREASE = 'counter/DECREASE';
 
+const INCREASE_ASYNC = 'counter/INCREASE_ASYNC';
+const DECREASE_ASYNC = 'counter/DECREASE_ASYNC';
+
 export const increase = createAction(INCREASE);
 export const decrease = createAction(DECREASE);
 
-// 1초 뒤에 increase 혹은 decrease 함수를 디스패치함
-export const increaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(increase());
-  }, 1000);
-};
+// 마우스 클릭 이벤트가 payload 안에 들어가지 않도록
+// () => undefined를 두 번째 파라미터로 넣어줍니다.
 
-export const decreaseAsync = () => (dispatch) => {
-  setTimeout(() => {
-    dispatch(decrease());
-  }, 1000);
-};
+export const increaseAsync = createAction(INCREASE_ASYNC, () => undefined);
+export const decreaseAsync = createAction(DECREASE_ASYNC, () => undefined);
+
+function* increaseSaga() {
+  yield delay(1000); // 1초를 기다립니다.
+  yield put(increase()); // 특정 액션을 디스패치합니다.
+  const number = yield select((state) => state.counter); // state는 스토어 상태를 의미함
+  console.log(`현재 값은 ${number}입니다.`);
+}
+
+function* decreaseSaga() {
+  yield delay(1000); // 1초를 기다린다.
+  yield put(decrease()); // 특정 액션을 디스패치한다.
+}
+
+export function* counterSaga() {
+  // takeEvery는 들어오는 모든 액션에 대해 특정 작업을 처리해 줍니다.
+  // throttle 두 번 클릭해도 되는이유 : 처음 클릭시 바로 호출 이후 3초간 여러번 요청 들어와도 3초 후 한번만 호출 따라서 두번클릭하면 두번찍힘
+  yield throttle(3000, INCREASE_ASYNC, increaseSaga);
+  // takeLatest는 기존에 진행 중이던 작업이 있다면 취소 처리하고
+  // 가장 마지막으로 실행된 작업만 수행합니다.
+  yield takeLatest(DECREASE_ASYNC, decreaseSaga);
+}
 
 const initialState = 0; // 상태는 곡 객체일 필요가 없습니다. 숫자도 작동해요.
 
